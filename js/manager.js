91162b7a1c68d9c9f814e34f7799f511c1d565f5
vasil <vasil.sokolov@gmail.com>
2016-12-01 10:55:52 +0300

    implemented async initialization of  admin panel tabs

--- js/manager.js
+++ js/manager.js
@@ -42,7 +42,15 @@ module.exports = function (oAppData) {
 			return {
 				start: function (ModulesManager) {
 					ModulesManager.run('AdminPanelWebclient', 'registerAdminPanelTab', [
-						function () { return require('modules/%ModuleName%/js/views/FilesAdminSettingsView.js'); },
+						function(resolve) {
+							require.ensure(
+								['modules/%ModuleName%/js/views/FilesAdminSettingsView.js'],
+								function() {
+									resolve(require('modules/%ModuleName%/js/views/FilesAdminSettingsView.js'));
+								},
+								"admin-bundle"
+							);
+						},
 						Settings.HashModuleName,
 						TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')
 					]);
