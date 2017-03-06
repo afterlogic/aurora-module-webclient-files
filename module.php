334a927948affa2142bd7a8bfc060b79d918afda
sash04ek <a.ovcharov@gmail.com>
2017-03-06 08:47:49 +0300

    refactoring namespaces

--- module.php
+++ module.php
@@ -18,9 +18,9 @@
  * @package Modules
  */
 
-namespace Aurora\Modules;
+namespace Aurora\Modules\FilesWebclient;
 
-class FilesWebclientModule extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractModule
 {
 	/**
 	 *
