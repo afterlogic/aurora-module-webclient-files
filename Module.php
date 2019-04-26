1e6a29b9fc793b1b6764e3a3fc560b6ef8c06fd7
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:38:21 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\FilesWebclient;
 
 /**
+ * This module displays the web interface for managing files.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
